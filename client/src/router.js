25434d7b429c1b79aa7ae62515de226e83627c50
Kyle Coberly <kyle.coberly@gmail.com>
2018-06-21 16:19:30 -0600

    Adding index page

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import Index from "@/views/index.vue";
+
 Vue.use(Router);
 
 export default new Router({
-	routes: [
-	]
+	routes: [{
+		path: "/",
+		name: "index",
+		component: Index
+	}]
 });
