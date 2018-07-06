2e3fc7882b4bcc3741dc11eb402368cd45a54f31
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-06 14:52:27 -0600

    Adding test environment

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,6 +3,13 @@ require("dotenv").load();
 module.exports = {
     development: {
         client: "pg",
-        connection: `postgres://kylecoberly@localhost/${process.env.DEVELOPMENT_DATABASE_NAME}`
+        connection: `postgres:///${process.env.DEVELOPMENT_DATABASE_NAME}`
+    },
+    test: {
+        client: "pg",
+        connection: `postgres:///${process.env.TEST_DATABASE_NAME}`,
+        seeds: {
+            directory: "./tests/seeds"
+        }
     }
 };
