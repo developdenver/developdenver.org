64291193b19638edfc76d9bbf7bcb1155fa99a3d
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-08 14:37:37 -0600

    Adding community page

---
+++ client/src/views/community.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="community">
+        <h2>Community</h2>
+        <ul class="profiles">
+            <li v-for="profile in profiles">
+                <profile-card
+                    :profile="profile"
+                />
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    import ProfileCard from "@/components/profile-card.vue";
+
+    export default {
+        components: {
+            ProfileCard
+        },
+        created(){
+            return this.$store.dispatch("getProfiles");
+        },
+        computed: {
+            profiles(){
+                return this.$store.state.profiles;
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    @import "@/styles/_typography.scss";
+
+    .community {
+        width: 100%;
+        h2 {
+            @include section-header-font;
+        }
+        .profiles {
+            display: flex;
+            flex-flow: row wrap;
+            li {
+                width: calc(100% * (1/4));
+            }
+        }
+    }
+</style>
