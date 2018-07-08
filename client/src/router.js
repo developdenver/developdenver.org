d94b3501aefa30084611c24a194bd4bf0c6ffa74
Kyle Coberly <kyle.coberly@gmail.com>
2018-07-08 12:56:15 -0600

    Adding privacy policy and code of conduct

--- client/src/router.js
+++ client/src/router.js
@@ -3,6 +3,8 @@ import Router from "vue-router";
 
 import Index from "@/views/index.vue";
 import Profile from "@/views/profile.vue";
+import CodeOfConduct from "@/views/code-of-conduct.vue";
+import PrivacyPolicy from "@/views/privacy-policy.vue";
 
 Vue.use(Router);
 
@@ -15,5 +17,13 @@ export default new Router({
 		path: "/profile",
 		name: "profile",
 		component: Profile
+	},{
+		path: "/code-of-conduct",
+		name: "code-of-conduct",
+		component: CodeOfConduct
+	},{
+		path: "/privacy-policy",
+		name: "privacy-policy",
+		component: PrivacyPolicy
 	}]
 });
