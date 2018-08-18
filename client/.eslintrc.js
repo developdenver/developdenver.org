cc310d734f1fb16d340bc79d3ad243463ab5a8c8
Kyle Coberly <kyle.coberly@gmail.com>
2018-08-18 10:36:27 -0600

    Adding linting and editor config

---
+++ client/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+	},
+	"extends": [
+		"plugin:vue/essential",
+		"@vue/standard",
+	],
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		indent: ["error", "tab", ],
+		quotes: ["error", "double", ],
+		"space-before-function-paren": ["error", "never", ],
+		"comma-dangle": ["error", "always", ],
+		"space-before-blocks": ["error", "always", ],
+		"semi": ["error", "always", ],
+		"no-tabs": 0,
+	},
+	parserOptions: {
+		parser: "babel-eslint",
+	},
+};
