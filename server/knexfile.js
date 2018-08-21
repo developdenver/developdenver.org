adb8320370c3095bdbd2528d02b67fd438bf3f16
Kyle Coberly <kyle.coberly@gmail.com>
2018-08-20 21:43:55 -0600

    Adding production connection information

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,10 +3,14 @@ require("dotenv").load();
 module.exports = {
     development: {
         client: "pg",
-        connection: `postgres:///${process.env.DEVELOPMENT_DATABASE_NAME}`
+        connection: `postgres:///${process.env.DEVELOPMENT_DATABASE_NAME}`,
     },
     test: {
         client: "pg",
-        connection: `postgres:///${process.env.TEST_DATABASE_NAME}`
-    }
+        connection: `postgres:///${process.env.TEST_DATABASE_NAME}`,
+    },
+    production: {
+        client: "pg",
+        connection: `${process.env.PRODUCTION_DATABASE_URL}?ssl=true`,
+    },
 };
