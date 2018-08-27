38de1ff96c18bdf2ea22cd2d3ab08017ec9e7b9f
Kyle Coberly <kyle.coberly@gmail.com>
2018-08-27 10:13:57 -0600

    Removing community page until we fix 404s

--- client/src/components/primary-navigation.vue
+++ client/src/components/primary-navigation.vue
@@ -13,9 +13,11 @@
             <li v-if="isAttendee">
                 <router-link :to="{name: 'news'}">News</router-link>
             </li>
+			<!--
             <li>
                 <router-link :to="{name: 'community'}">Community</router-link>
             </li>
+			-->
             <li v-if="isLoggedIn">
                 <router-link :to="{name: 'my-profile'}">My Profile</router-link>
             </li>
