c41fb4ca0637f0ffa78d59e9bb531c4026b8b73d
Cassandra Torske <cassandra.torske@gmail.com>
2018-09-04 17:17:44 -0600

    added isticketholder middleware

--- server/src/routes/talks.js
+++ server/src/routes/talks.js
@@ -7,12 +7,22 @@ const {
   create, read, update, destroy, list, 
 } = require("../controllers/talk");
 
+function isTicketHolder(request, response, next) {
+  if (request.user && request.user.ticket_level !== null) {
+    next();
+  } else {
+    const error = new Error("You must have a ticket to vote.");
+    error.status = 401;
+    next(error);
+  }
+}
+
 module.exports = (app) => {
   router.get("/", list);
   router.get("/:title", read);
   router.use(passport.authenticate("jwt", { session: false }));
-  router.post("/:id/vote", VoteController.vote);
-  router.post("/:id/unvote", VoteController.unvote);
+  router.post("/:id/vote", isTicketHolder, VoteController.vote);
+  router.post("/:id/unvote", isTicketHolder, VoteController.unvote);
   router.post("/", create);
   router.put("/:id", update);
   router.delete("/:id", destroy);
