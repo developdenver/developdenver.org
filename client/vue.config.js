7d5e920fe4315791ce3015e987200b2d11a5052f
Kyle Coberly <kyle.coberly@gmail.com>
2018-09-24 21:31:59 -0600

    Updating network caching strategy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,2 +1,14 @@
+const manifestJSON = require("./public/manifest.json");
+
 module.exports = {
+	pwa: {
+		themeColor: manifestJSON.theme_color,
+		name: manifestJSON.short_name,
+		msTileColor: manifestJSON.background_color,
+		workboxPluginMode: "GenerateSW",
+		workboxOptions: {
+			urlPattern: "/*/",
+			handler: "networkFirst",
+		},
+	},
 };
