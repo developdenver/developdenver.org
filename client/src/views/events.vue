0ac43c6caf16aff1a4ff0864cabaeeb386682421
Cassandra Torske <cassandra.torske@gmail.com>
2018-10-02 20:14:29 -0600

    add sponsor component into events and adjust styles to account for it

--- client/src/views/events.vue
+++ client/src/views/events.vue
@@ -1,51 +1,55 @@
 <template>
-	<section class="events">
-		<h2>Events</h2>
-		<div class="list-wrapper">
-			<header>
-				<h3>Talks</h3>
-			</header>
-			<event-card-list
-				:events="talks"
-			/>
-			<header>
-				<h3>Panels</h3>
-			</header>
-			<event-card-list
-				:events="panels"
-			/>
-			<header>
-				<h3>Lightning Talks</h3>
-			</header>
-			<event-card-list
-				:events="lightningTalks"
-			/>
-			<header>
-				<h3>Workshops</h3>
-			</header>
-			<event-card-list
-				:events="workshops"
-			/>
-			<header>
-				<h3>Live Performances</h3>
-			</header>
-			<event-card-list
-				:events="performances"
-			/>
-			<header>
-				<h3>Featuring</h3>
-			</header>
-			<ul class="services">
-				<li v-for="(service, index) in services" :key="index">
-					{{service.properties.title}}
-				</li>
-			</ul>
-		</div>
-	</section>
+	<div>
+		<section class="events">
+			<h2>Events</h2>
+			<div class="list-wrapper">
+				<header>
+					<h3>Talks</h3>
+				</header>
+				<event-card-list
+					:events="talks"
+				/>
+				<header>
+					<h3>Panels</h3>
+				</header>
+				<event-card-list
+					:events="panels"
+				/>
+				<header>
+					<h3>Lightning Talks</h3>
+				</header>
+				<event-card-list
+					:events="lightningTalks"
+				/>
+				<header>
+					<h3>Workshops</h3>
+				</header>
+				<event-card-list
+					:events="workshops"
+				/>
+				<header>
+					<h3>Live Performances</h3>
+				</header>
+				<event-card-list
+					:events="performances"
+				/>
+				<header>
+					<h3>Featuring</h3>
+				</header>
+				<ul class="services">
+					<li v-for="(service, index) in services" :key="index">
+						{{service.properties.title}}
+					</li>
+				</ul>
+			</div>
+		</section>
+		<sponsor-list />		
+	</div>
 </template>
 
 <script>
 import EventCardList from "@/components/event-card-list";
+import SponsorList from "@/components/sponsor-list";
 import workshops from "@/data/workshops";
 import services from "@/data/services";
 import performances from "@/data/performances";
@@ -54,6 +58,7 @@ import {shuffle} from "@/utilities/shuffle";
 export default {
 	components: {
 		EventCardList,
+		SponsorList,
 	},
 	created() {
 		this.$store.dispatch("events/fetchEvents");
@@ -88,8 +93,10 @@ export default {
 @import "@/styles/_typography.scss";
 @import "@/styles/_sizes.scss";
 
+
 .events {
 	max-width: 600px;
+	margin-left: 25vw;
 	display: flex;
 	flex-flow: column nowrap;
 	flex: 1;
