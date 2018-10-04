187a12e64f9357dbcf89061ea2ba4df6f8dbcfc2
Kyle Coberly <kyle.coberly@gmail.com>
2018-10-04 09:54:23 -0600

    Updating event model

--- server/src/models/event.js
+++ server/src/models/event.js
@@ -6,7 +6,7 @@ class Event extends Model {
         this.publicProperties = [
             "id", "title", "type", "description", "talk_photo_url",
             "author_id", "author_first_name", "author_photo_url",
-            "author_last_name",
+            "author_last_name", "category"
         ]
     }
     find(id){
@@ -20,6 +20,7 @@ class Event extends Model {
                 "talk.talk_photo_url as talk_photo_url",
                 "profile.id as author_id", "profile.first_name as first_name",
                 "profile.last_name as last_name", "profile.profile_photo_url",
+                "talk.category as category"
             ).innerJoin("profile", "talk.user_id", "profile.id")
             .where("talk.is_accepted", true);
     }
