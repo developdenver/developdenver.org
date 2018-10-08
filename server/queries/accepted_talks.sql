4ba59f553789fdfeb8b9586b724d3810747950d5
Kyle Coberly <kyle.coberly@gmail.com>
2018-10-08 00:03:39 -0600

    Updating accepted talk query to include talk id

--- server/queries/accepted_talks.sql
+++ server/queries/accepted_talks.sql
@@ -1,4 +1,4 @@
-select profile.id, talk.title, profile.first_name, profile.last_name, profile.email as votes
+select talk.id, talk.title, profile.id, profile.first_name, profile.last_name, profile.email as votes
 from talk
 left join profile on talk.user_id = profile.id
 where is_accepted is true;
