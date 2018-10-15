80ff2776ea3e795484032a84164296b6b1449be3
Kyle Coberly <kyle.coberly@gmail.com>
2018-10-14 23:48:26 -0600

    Adding schedule filters

--- client/src/views/schedule.vue
+++ client/src/views/schedule.vue
@@ -1,5 +1,27 @@
 <template>
 	<div class="schedule-wrapper">
+		<div class="filters">
+			<div class="filter category-filter">
+				<label>Filter by Category:</label>
+				<select v-model="selectedCategory">
+					<option
+						v-for="(category, index) in categories"
+						:key="index"
+						:value="category.id"
+					>{{category.label}}</option>
+				</select>
+			</div>
+			<div class="filter venue-filter">
+				<label>Filter by Venue:</label>
+				<select v-model="selectedVenue">
+					<option
+						v-for="(venue, index) in venues"
+						:value="venue.id"
+						:key="index"
+					>{{venue.label}}</option>
+				</select>
+			</div>
+		</div>
 		<section class="schedule">
 			<h2>Thursday</h2>
 			<schedule-card-list :events="thursdayEventsByTime" />
@@ -17,6 +39,58 @@ import ScheduleCardList from "@/components/schedule-card-list";
 import SponsorList from "@/components/sponsor-list";
 
 export default {
+	data() {
+		return {
+			selectedCategory: "all",
+			categories: [{
+				id: "all",
+				label: "All",
+				includedCategories: ["development", "product", "careers", "devops", "workshop", "event"],
+			}, {
+				id: "development",
+				label: "Development",
+				includedCategories: ["development", "workshop", "event"],
+			}, {
+				id: "product",
+				label: "Product",
+				includedCategories: ["product", "workshop", "event"],
+			}, {
+				id: "careers",
+				label: "Careers",
+				includedCategories: ["careers", "workshop", "event"],
+			}, {
+				id: "devops",
+				label: "DevOps",
+				includedCategories: ["devops", "workshop", "event"],
+			}],
+			selectedVenue: "all",
+			venues: [{
+				id: "all",
+				label: "All",
+				includedVenues: ["Bigsby's Folly", "Urban Cyclist", "Catalyst HTI", "Big Trouble", "The Source Hotel"],
+			}, {
+				id: "Bigsby's Folly",
+				label: "Bigsby's Folly",
+				includedVenues: ["Bigsby's Folly"],
+			}, {
+				id: "Urban Cyclist",
+				label: "Urban Cyclist",
+				includedVenues: ["Urban Cyclist"],
+			}, {
+				id: "Catalyst HTI",
+				label: "Catalyst HTI",
+				includedVenues: ["Catalyst HTI"],
+			}, {
+				id: "Big Trouble",
+				label: "Big Trouble",
+				includedVenues: ["Big Trouble"],
+			}, {
+				id: "The Source Hotel",
+				label: "The Source Hotel",
+				includedVenues: ["The Source Hotel"],
+			}],
+		};
+	},
 	components: {
 		SponsorList,
 		ScheduleCardList,
@@ -35,11 +109,21 @@ export default {
 			const events = [...this.$store.state.scheduleListings.list];
 			return events.sort(this.sortEvents);
 		},
+		filteredEvents() {
+			return this.events
+				.filter(event => this.filterByCategory(this.selectedCategory)(event.properties.category))
+				.filter(event => this.filterByVenue(this.selectedVenue)(event.properties.venue));
+		},
 		thursdayEventsByTime() {
-			return this.events.filter(event => event.properties.day === "Thursday");
+			return this.filteredEvents.filter(event => event.properties.day === "Thursday");
 		},
 		fridayEventsByTime() {
-			return this.events.filter(event => event.properties.day === "Friday");
+			return this.filteredEvents.filter(event => event.properties.day === "Friday");
+		},
+		getCategory(category) {
+			return events => {
+				events.filter(event => event.properties.category === category);
+			};
 		},
 		thursdayEventsByVenue() {
 			return this.splitByVenues(this.events.filter(event => event.properties.day === "Thursday"));
@@ -54,6 +138,14 @@ export default {
 		},
 		compareNumbers(firstNumber, secondNumber) {
 			return Number(firstNumber) - Number(secondNumber);
+		},
+		filterByCategory(selectedCategory) {
+			return eventCategory => this.categories
+				.find(category => category.id === selectedCategory).includedCategories.includes(eventCategory);
+		},
+		filterByVenue(selectedVenue) {
+			return eventVenue => this.venues
+				.find(venue => venue.id === selectedVenue).includedVenues.includes(eventVenue);
 		},
 		splitByVenues(events) {
 			return [{
@@ -80,12 +172,38 @@ export default {
 <style lang="scss">
 @import "@/styles/_typography.scss";
 @import "@/styles/_sizes.scss";
+@import "@/styles/_colors.scss";
 
 .schedule-wrapper {
 	width: 100%;
 	display: flex;
 	flex-flow: column nowrap;
 	align-items: center;
+	.filters {
+		position: sticky;
+		top: 0;
+		background-color: $white;
+		z-index: 50;
+		padding: $baseline;
+		opacity: 0.97;
+		.filter {
+			display: flex;
+			flex-flow: row nowrap;
+			justify-content: space-between;
+			align-items: center;
+			margin-bottom: $small;
+			width: 100%;
+			label, select {
+				display: inline;
+			}
+			label {
+				margin-right: $baseline;
+			}
+			select {
+				min-width: 10rem;
+			}
+		}
+	}
 }
 .schedule {
 	max-width: 600px;
