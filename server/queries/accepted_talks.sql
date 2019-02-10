e0843734d8d194c3c1be04b2e2d76d62cf6c07d8
Kyle Coberly <kyle.coberly@gmail.com>
2019-02-09 19:18:40 -0700

    Deleting queries

--- server/queries/accepted_talks.sql
+++
@@ -1,4 +0,0 @@
-select talk.id, talk.title, profile.id, profile.first_name, profile.last_name, profile.email as votes
-from talk
-left join profile on talk.user_id = profile.id
-where is_accepted is true;
