4516b0529d5a58cdb3fc385081a40bb65e74eacc
Brian Schiller <bgschiller@gmail.com>
2019-02-10 13:53:59 -0700

    baby's first commit
    I removed some text on the main page

--- client/src/data/feature-benefits.js
+++ client/src/data/feature-benefits.js
@@ -1,14 +1,17 @@
-export default [{
-	heading: "Community first",
-	image: {
-		url: "./img/toast.jpeg",
-		altText: "People having fun",
-	},
-	copy: `
+export default [
+	{
+		heading: "Community first",
+		image: {
+			url: "./img/toast.jpeg",
+			altText: "People having fun"
+		},
+		copy: `
 Over the last 8 years Develop Denver has become more than a conference. It’s been a place to learn and grow professionally, make new friends and have a great time. It’s been the starting place for great debates on technology and philosophy around creating things for our digital world.
 
 Develop Denver exists to inspire and build community. It also has an ability to blow minds and introduce people to new concepts, languages, & frameworks.
 
-Topics ranging from design, dev ops, frontend, backend, UX, strategy and more there is something for everyone. Talks are submitted by speakers and voted on by the community. The motto is what you put in is what you take out.
+Topics ranging from design, dev ops, frontend, backend, UX, strategy and more there is something for everyone. Talks are submitted by speakers and voted on by the community.${" The motto is what you put in is what you take out." &&
+			""}
 `
-} ];
+	}
+];
