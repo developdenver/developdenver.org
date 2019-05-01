5b2c89c574502f8ebb1e8db098c7960890132ba9
Brian Schiller <bgschiller@gmail.com>
2019-04-30 20:22:25 -0600

    Add featured talks

--- client/src/components/talk-info.vue
+++ client/src/components/talk-info.vue
@@ -1,89 +1,87 @@
 <template>
-    <section class="talk">
-        <img :src="talk.properties.talkPhotoUrl" alt="Talk photo" class="talk-image" />
-        <div class="talk-details">
-            <h2>{{talk.properties.title}}</h2>
-            <div class="description">
-				<p class="author">
-					<router-link
-						:to="{
+  <section class="talk">
+    <div class="talk-details">
+      <h2>{{talk.properties.title}}</h2>
+      <div class="description">
+        <p class="author">
+          <router-link
+            :to="{
 							name: 'profile',
 							params: {
 								id: talk.properties.authorId,
 							},
 						}"
-					>
-					{{talk.properties.firstName}} {{talk.properties.lastName}}
-					</router-link>
-				</p>
-                <div class="description-html" v-html="descriptionHtml"></div>
-                <p class="talk-type">Type: {{talkLabel}}</p>
-            </div>
-        </div>
-    </section>
+          >{{talk.properties.firstName}} {{talk.properties.lastName}}</router-link>
+        </p>
+        <div class="description-html" v-html="descriptionHtml"></div>
+        <p class="talk-type">Type: {{talkLabel}}</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Showdown from "showdown";
+import Showdown from 'showdown';
 const showdown = new Showdown.Converter();
 
 export default {
-	data() {
-		return {
-			talkTypes: [{
-				label: "Talk (30 - 45 minutes)",
-				value: "talk",
-			}, {
-				label: "Lightning Talk (5 - 10 minutes)",
-				value: "lightning",
-			}, {
-				label: "Panel (30 - 45 minutes, multiple people)",
-				value: "panel",
-			}, {
-				label: "Workshop",
-				value: "workshop",
-			}, {
-				label: "Event",
-				value: "event",
-			}, {
-				label: "Performance",
-				value: "performance",
-			}],
-		};
-	},
-	props: {
-		talk: {
-			type: Object,
-			required: true,
-		},
-	},
-	computed: {
-		image() {
-			return {
-				url: this.talk.properties.talk_photo_url,
-				altText: "Talk photo",
-			};
-		},
-		descriptionHtml() {
-			return this.talk.properties.description
-				? showdown.makeHtml(this.talk.properties.description)
-				: "";
-		},
-		talkLabel() {
-			return this.talk.properties.type
-				? this.talkTypes
-					.find(type => type.value === this.talk.properties.type)
-					.label
-				: "";
-		},
-	},
+    data() {
+        return {
+            talkTypes: [
+                {
+                    label: 'Talk (30 - 45 minutes)',
+                    value: 'talk',
+                },
+                {
+                    label: 'Lightning Talk (5 - 10 minutes)',
+                    value: 'lightning',
+                },
+                {
+                    label: 'Panel (30 - 45 minutes, multiple people)',
+                    value: 'panel',
+                },
+                {
+                    label: 'Workshop',
+                    value: 'workshop',
+                },
+                {
+                    label: 'Event',
+                    value: 'event',
+                },
+                {
+                    label: 'Performance',
+                    value: 'performance',
+                },
+            ],
+        };
+    },
+    props: {
+        talk: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        descriptionHtml() {
+            return this.talk.properties.description
+                ? showdown.makeHtml(this.talk.properties.description)
+                : '';
+        },
+        talkLabel() {
+            return this.talk.properties.type
+                ? this.talkTypes.find(
+                      type => type.value === this.talk.properties.type,
+                  ).label
+                : '';
+        },
+    },
 };
 </script>
 
 <style lang="scss">
-@import "@/styles/_typography.scss";
-@import "@/styles/_sizes.scss";
-@import "@/styles/_colors.scss";
+@import '@/styles/_typography.scss';
+@import '@/styles/_sizes.scss';
+@import '@/styles/_colors.scss';
 
 $clip-amount: 80px;
 
@@ -93,9 +91,9 @@ $clip-amount: 80px;
     flex-flow: row nowrap;
     align-items: center;
     justify-content: flex-end;
-	.talk-image {
-		max-height: 100vh;
-	}
+    .talk-image {
+        max-height: 100vh;
+    }
     .talk-details {
         order: 1;
         margin-top: $xl * -1;
@@ -103,7 +101,7 @@ $clip-amount: 80px;
         background-color: $pure-white;
         padding: $large;
         z-index: 10;
-		min-width: 40%;
+        min-width: 40%;
         h2 {
             @include section-header-font;
         }
@@ -117,25 +115,31 @@ $clip-amount: 80px;
             a {
                 color: $primary-color;
             }
-			.talk-type {
-				@include italic-body-font;
-			}
-			.description-html {
-				line-height: 1.5;
-				h1, h2, h3, h4, h5, h6 {
-					@include fieldset-header-font;
-				}
-				ul, ol {
-					list-style-type: circle;
-					margin-bottom: $baseline;
-				}
-				strong {
-					font-weight: 700;
-				}
-				em {
-					font-style: italic;
-				}
-			}
+            .talk-type {
+                @include italic-body-font;
+            }
+            .description-html {
+                line-height: 1.5;
+                h1,
+                h2,
+                h3,
+                h4,
+                h5,
+                h6 {
+                    @include fieldset-header-font;
+                }
+                ul,
+                ol {
+                    list-style-type: circle;
+                    margin-bottom: $baseline;
+                }
+                strong {
+                    font-weight: 700;
+                }
+                em {
+                    font-style: italic;
+                }
+            }
         }
     }
     img {
@@ -143,38 +147,38 @@ $clip-amount: 80px;
         clip-path: inset($clip-amount 0);
         max-width: 60%;
     }
-	@media (max-width: $medium-breakpoint) {
-		flex-flow: column nowrap;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
+    @media (max-width: $medium-breakpoint) {
+        flex-flow: column nowrap;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
         img {
             order: 1;
             width: 100%;
-			clip-path: none;
+            clip-path: none;
         }
-		.talk-details {
-			width: 100%;
-			margin: 0;
-		}
-	}
-    @media (max-width: $large-breakpoint){
+        .talk-details {
+            width: 100%;
+            margin: 0;
+        }
+    }
+    @media (max-width: $large-breakpoint) {
         align-self: auto;
         flex-flow: column nowrap;
         align-self: flex-start;
-		justify-content: center;
+        justify-content: center;
         width: 100%;
-		margin-top: 0;
-		margin-bottom: $large;
-		img {
+        margin-top: 0;
+        margin-bottom: $large;
+        img {
             order: 1;
-			display: block;
-			width: 100%;
-			clip-path: none;
-		}
-		.talk-details {
-			margin-right: 0;
-		}
+            display: block;
+            width: 100%;
+            clip-path: none;
+        }
+        .talk-details {
+            margin-right: 0;
+        }
     }
 }
 </style>
--- client/src/views/index.vue
+++ client/src/views/index.vue
@@ -4,44 +4,57 @@
       <hero-banner/>
     </section>
     <section class="ticket-info">
-		<feature-benefit
-		  v-for="featureBenefit in featureBenefits"
-		  :key="featureBenefit.heading"
-		  v-bind="featureBenefit"
-		/>
+      <feature-benefit
+        v-for="featureBenefit in featureBenefits"
+        :key="featureBenefit.heading"
+        v-bind="featureBenefit"
+      />
       <div class="ticket-details">
         <h2>Conference Details</h2>
         <p>When: August 15 and 16, 2019</p>
         <p>Where: RiNo Brighton Boulevard</p>
-	  </div>
+      </div>
 
       <div class="ticket-details">
-		<h2>Venues</h2>
-		<venue-list />
-	  </div>
+        <h2>Venues</h2>
+        <venue-list/>
+      </div>
 
       <div class="ticket-details">
-		<h2>Conference Badges</h2>
-		<p>Conference badges are now on sale!</p>
-		<p>All badges include access to:</p>
-		<ul>
-		  <li>Custom printed 2-day conference badge</li>
-		  <li>Access to keynote, talks, workshops, and live podcasts</li>
-		  <li>Coffee, Breakfast, and Lunch at Zeppelin Station vendors both days</li>
-		  <li>Opening and closing parties</li>
-		  <li>8th annual Ballmer Peak Hackathon</li>
-		</ul>
-	  </div>
+        <h2>Conference Badges</h2>
+        <p>Conference badges are now on sale!</p>
+        <p>All badges include access to:</p>
+        <ul>
+          <li>Custom printed 2-day conference badge</li>
+          <li>Access to keynote, talks, workshops, and live podcasts</li>
+          <li>Coffee, Breakfast, and Lunch at Zeppelin Station vendors both days</li>
+          <li>Opening and closing parties</li>
+          <li>8th annual Ballmer Peak Hackathon</li>
+        </ul>
+      </div>
 
       <div class="ticket-details">
-		<h3>Pricing</h3>
-		<p>Early Bird - $149 <em>($299 after May 1st)</em></p>
-		<p>Employer - $399 <em>(Help make Develop Denver affordable to all attendees)</em></p>
+        <h3>Pricing</h3>
+        <p>
+          Early Bird - $149
+          <em>($299 after May 1st)</em>
+        </p>
+        <p>
+          Employer - $399
+          <em>(Help make Develop Denver affordable to all attendees)</em>
+        </p>
 
-		<p>Don’t worry, we’re all setup so you can purchase a group of conference badges. You’ll be asked to provide an email address for each attendee and you’re good to go. Have questions? Please contact us at <a href="mailto:hello@developdenver.org">hello@developdenver.org</a> if you need help.</p>
-		<router-link class="button" :to="{name: 'tickets'}">Purchase Badge</router-link>
-	  </div>
+        <p>
+          Don’t worry, we’re all setup so you can purchase a group of conference badges. You’ll be asked to provide an email address for each attendee and you’re good to go. Have questions? Please contact us at
+          <a
+            href="mailto:hello@developdenver.org"
+          >hello@developdenver.org</a> if you need help.
+        </p>
+        <router-link class="button" :to="{name: 'tickets'}">Purchase Badge</router-link>
+      </div>
     </section>
+    <FeaturedTalks/>
+    <CallForTalks/>
     <sponsor-list/>
   </div>
 </template>
@@ -53,6 +66,7 @@ import FeatureBenefit from '@/components/feature-benefit.vue';
 import SponsorList from '@/components/sponsor-list';
 import CallForTalks from '@/components/call-for-talks';
 import VenueList from '@/components/venue-list';
+import FeaturedTalks from '@/components/featured-talks.vue';
 
 import boardProfiles from '@/data/board-profiles';
 import featureBenefits from '@/data/feature-benefits';
@@ -64,7 +78,8 @@ export default {
         FeatureBenefit,
         SponsorList,
         CallForTalks,
-		VenueList,
+        VenueList,
+        FeaturedTalks,
     },
     computed: {
         boardProfiles() {
@@ -128,9 +143,9 @@ export default {
         display: flex;
         flex-direction: column;
         justify-content: center;
-		align-items: center;
-		max-width: $max-line-length;
-		padding: $baseline;
+        align-items: center;
+        max-width: $max-line-length;
+        padding: $baseline;
         .button {
             @include call-to-action-button;
             margin: 0 auto;
@@ -138,7 +153,7 @@ export default {
         h2 {
             @include section-header-font;
             width: 100%;
-			align-self: flex-start;
+            align-self: flex-start;
         }
         p {
             @include stylized-body-font;
@@ -148,20 +163,20 @@ export default {
             display: flex;
             flex-flow: column nowrap;
             align-items: flex-start;
-			width: 100%;
-			margin-bottom: $large;
-			h3 {
-				@include tertiary-header-font;
-				width: 100%;
-				align-self: flex-start;
-			}
-			ul {
-				list-style-type: circle;
-				max-width: 79vw;
-				li {
-					margin-bottom: $small;
-				}
-			}
+            width: 100%;
+            margin-bottom: $large;
+            h3 {
+                @include tertiary-header-font;
+                width: 100%;
+                align-self: flex-start;
+            }
+            ul {
+                list-style-type: circle;
+                max-width: 79vw;
+                li {
+                    margin-bottom: $small;
+                }
+            }
         }
     }
 }
