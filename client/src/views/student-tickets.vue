781ea22bf88ca7aa8d1ed3c92f7077a21efe3c08
Kyle Coberly <kyle.coberly@gmail.com>
2019-07-06 20:48:34 -0600

    Styling discount tickets

--- client/src/views/student-tickets.vue
+++ client/src/views/student-tickets.vue
@@ -1,6 +1,16 @@
 <template>
   <section class="student-tickets">
+	<countdown />
     <h2>Tickets</h2>
+	<div class="ticket-details">
+		<div class="date-and-location">
+			<p>When: August 15 and 16, 2019</p>
+			<p>Where: RiNo Brighton Boulevard</p>
+		</div>
+		<div class="benefits">
+			<p>All badges include a custom printed 2-day conference badge, access to keynote, talks, workshops, and live podcasts, coffee, breakfast, and lunch at Zeppelin Station vendors both days, opening and closing parties, 8th annual Ballmer Peak Hackathon</p>
+		</div>
+	</div>
     <ticket-purchase :levels="levels" :isDiscountCode="true"/>
   </section>
 </template>
@@ -9,11 +19,13 @@
 /* global Stripe */
 import CreditCardPayment from '../components/credit-card-payment';
 import TicketPurchase from '../components/ticket-purchase';
+import Countdown from '@/components/count-down';
 
 export default {
     components: {
         CreditCardPayment,
         TicketPurchase,
+		Countdown,
     },
     data() {
         return {
@@ -36,5 +48,37 @@ export default {
 @import '@/styles/_sizes.scss';
 
 .student-tickets {
+	@include grid;
+	@media (max-width: $small-breakpoint) {
+		padding: $baseline;
+	}
+	.ticket-details {
+		@include grid-full-width;
+		@include grid;
+		.date-and-location {
+			@include grid-text-0;
+		}
+		.benefits {
+			@include grid-text-1;
+		}
+		.groups {
+			@include grid-text-2;
+		}
+	}
+	.venues, .ticket-purchase {
+		@include grid-full-width;;
+	}
+	.ticket-details, .venues {
+		margin-bottom: $baseline * 8;
+		@media (max-width: $small-breakpoint) {
+			margin-bottom: $baseline * 2;
+		}
+	}
+	.countdown {
+		@include grid-countdown;
+		@media (max-width: $small-breakpoint) {
+			display: none;
+		}
+	}
 }
 </style>
