f31b7e37643d6bc7e9e6de8a48524420c866a86a
Liz Henderson <lhenderson303@gmail.com>
2020-01-21 11:35:55 -0700

    app-header.vue update styles and cleanup component

--- client/src/components/app-header.vue
+++ client/src/components/app-header.vue
@@ -1,23 +1,23 @@
 <template>
 	<header class="app-header">
 		<div class="app-header-content">
-			<router-link to="/" class="circle-logo-link"><circle-logo /></router-link>
-			<button
-				class="menu"
-				@click="toggleMenu"
-				:isMenuOpen="isMenuOpen"
-			><font-awesome-icon :icon="menuIcon" /></button>
-			<router-link to="/">
-				<h1>DVLP / DNVR / 2019</h1>
-			</router-link>
+			<router-link to="/" class="circle-logo-link"
+				><circle-logo
+			/></router-link>
+			<button class="menu" @click="toggleMenu" :isMenuOpen="isMenuOpen">
+				<font-awesome-icon :icon="menuIcon" />
+			</button>
+			<div class="title-wrap">
+				<img src="/img/DVLP_DNVR_text.png" />
+			</div>
 		</div>
 		<slot />
 	</header>
 </template>
 
 <script>
-import CircleLogo from "@/components/circle-logo";
-import PrimaryNavigation from "@/components/primary-navigation.vue";
+import CircleLogo from '@/components/circle-logo';
+import PrimaryNavigation from '@/components/primary-navigation.vue';
 
 export default {
 	components: {
@@ -28,91 +28,91 @@ export default {
 		isMenuOpen: Boolean,
 	},
 	computed: {
-		menuIcon(){
-			return this.isMenuOpen ? "times" : "bars"
+		menuIcon() {
+			return this.isMenuOpen ? 'times' : 'bars';
 		},
 	},
 	methods: {
-		toggleMenu(){
-			this.$emit("toggleMenu");
-		}
-	}
+		toggleMenu() {
+			this.$emit('toggleMenu');
+		},
+	},
 };
 </script>
 
 <style lang="scss">
-	@import "@/styles/_sizes.scss";
-	@import "@/styles/_colors.scss";
-	@import "@/styles/_typography.scss";
+@import '@/styles/_flex.scss';
+@import '@/styles/_general.scss';
+@import '@/styles/_colors.scss';
+@import '@/styles/_sizes.scss';
+@import '@/styles/_typography.scss';
 
-	.app-header {
-		.app-header-content {
-			background-color: $black;
-			position: fixed;
-			max-width: 100px; // Chrome hack
-			height: 100vh;
-			top: 0;
-			left: 0;
-			z-index: 100;
-			padding: $baseline;
-			@media (max-width: $small-breakpoint) {
-				top: auto;
-				left: auto;
-				height: auto;
-				max-width: 100%;
-				width: 100%;
-				bottom: 0;
-				display: flex;
-				flex-flow: row nowrap;
-				justify-content: space-between;
-				.circle-logo-link {
-					display: none;
-				}
-				.hamburger-container {
-					display: none;
-				}
-				a {
-					width: 100%;
-				}
+.app-header {
+	.app-header-content {
+		background-color: $yellow;
+		border-right: $thin-border-width solid $black;
+		height: 100vh;
+		left: 0;
+		max-width: 100px; // Chrome hack
+		position: fixed;
+		top: 0;
+		z-index: 100;
+		@media (max-width: $small-breakpoint) {
+			@include flexbox;
+			@include flex-flow(nowrap);
+			@include justify-content(space-between);
+			bottom: 0;
+			height: auto;
+			left: auto;
+			max-width: 100%;
+			top: auto;
+			width: 100%;
+			.circle-logo-link {
+				display: none;
 			}
-			.circle-logo {
-			}
-			h1 {
-				@include primary-header-font;
-				writing-mode: vertical-rl;
-				transform: rotate(180deg);
-				@media (max-width: $small-breakpoint) {
-					writing-mode: initial;
-					transform: initial;
-					font-size: $baseline;
-				}
+			.hamburger-container {
+				display: none;
 			}
 		}
-		.menu {
-			display: inline-flex;
-			justify-content: center;
-			align-items: center;
-			padding: 0;
-			background: none;
-			border: none;
-			color: $white;
-			width: 100%;
-			cursor: pointer;
-			margin-top: $baseline * 4;
-			margin-bottom: $baseline * 4;
-			@media (max-width: $small-breakpoint) {
-				margin: 0;
-				order: 2;
-				flex: 1;
-				justify-self: flex-end;
-			}
-			svg {
-				width: 100%;
-				height: 36px;
-				@media (max-width: $small-breakpoint) {
-					width: auto;
-				}
+		.title-wrap {
+			@include flexbox;
+			@include justify-content(center);
+			@include align-items(center);
+			height: 78%;
+			img {
+				height: 100%;
+				max-height: 400px;
+				width: auto;
 			}
 		}
 	}
+	.menu {
+		@include align-items(center);
+		@include justify-content(center);
+		cursor: pointer;
+		display: inline-flex;
+		background: none;
+		border-bottom: $thin-border-width solid $black;
+		border-left: none;
+		border-right: none;
+		border-top: none;
+		color: $black;
+		width: 100%;
+		padding-top: $baseline;
+		padding-bottom: $baseline;
+		@media (max-width: $small-breakpoint) {
+			@include flex(1);
+			justify-self: flex-end;
+			margin: 0;
+			order: 2;
+		}
+		svg {
+			width: 100%;
+			height: 36px;
+			@media (max-width: $small-breakpoint) {
+				width: auto;
+			}
+		}
+	}
+}
 </style>
