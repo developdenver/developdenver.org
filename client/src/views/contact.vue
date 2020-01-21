8525bd8f670cad57d00319912c06d66a85044086
Liz Henderson <lhenderson303@gmail.com>
2020-01-21 12:14:17 -0700

    add contact.vue

---
+++ client/src/views/contact.vue
@@ -0,0 +1,8 @@
+<template>
+	<section class="contact">
+		<div class="details">
+			<h2>Contact</h2>
+			<a href="mailto:hello@developdenver.org">hello@developdenver.org</a>
+		</div>
+	</section>
+</template>
