1808ba081b9c1fd72ef34c6980af9675e9cba8ef
Liz Henderson <lhenderson303@gmail.com>
2020-01-21 12:13:19 -0700

    clenup router.js

--- client/src/router.js
+++ client/src/router.js
@@ -3,37 +3,38 @@ import Router from 'vue-router';
 
 import store from './store';
 
-import Index from '@/views/index.vue';
-import EditProfile from '@/views/profile.vue';
-import Community from '@/views/community.vue';
+import About from '@/views/about';
 import CodeOfConduct from '@/views/code-of-conduct.vue';
+import ClaimTicket from '@/views/claim-ticket';
+import Contact from '@/views/contact.vue';
+import DiscountTickets from '@/views/discount-tickets';
+import EditProfile from '@/views/profile.vue';
+import Events from '@/views/events';
+import Index from '@/views/index.vue';
+import LetterToBoss from '@/views/letter-to-boss';
+import Login from '@/views/login';
+import MyTickets from '@/views/my-tickets';
 import PrivacyPolicy from '@/views/privacy-policy.vue';
 import ProfilePage from '@/views/profile-page.vue';
 import Register from '@/views/register';
-import Login from '@/views/login';
-import StudentTickets from '@/views/student-tickets';
-import DiscountTickets from '@/views/discount-tickets';
-import Tickets from '@/views/tickets';
-import About from '@/views/about';
-import News from '@/views/news';
 import ResetPassword from '@/views/reset-password';
 import RequestReset from '@/views/request-reset';
-//import SubmitTalk from '@/views/submit-talk';
-import TalkPage from '@/views/talk-page';
-import ScheduleListingPage from '@/views/schedule-listing-page';
-import WorkshopPage from '@/views/workshop-page';
-//import Talks from '@/views/talks';
-import UserTalk from '@/views/user-talk';
-import Events from '@/views/events';
 //import Schedule from '@/views/schedule';
-import MyTickets from '@/views/my-tickets';
-import ClaimTicket from '@/views/claim-ticket';
+import ScheduleListingPage from '@/views/schedule-listing-page';
+import StudentTickets from '@/views/student-tickets';
 import Sponsorship from '@/views/sponsorship';
-import LetterToBoss from '@/views/letter-to-boss';
+import SubmitTalk from '@/views/submit-talk';
+import Talks from '@/views/talks';
+import TalkPage from '@/views/talk-page';
+import Tickets from '@/views/tickets';
+import UserTalk from '@/views/user-talk';
+import WorkshopPage from '@/views/workshop-page';
 
 Vue.use(Router);
 
 const ticketsOnSale = true;
+
+/* Add new routes in alphabetical order */
 
 const router = new Router({
 	mode: 'history',
@@ -44,12 +45,54 @@ const router = new Router({
 			component: Index,
 		},
 		{
-			path: '/register',
-			name: 'register',
-			component: Register,
+			path: '/about',
+			name: 'about',
+			component: About,
+		},
+		{
+			path: '/claim-ticket/:claimToken',
+			name: 'claim-ticket',
+			component: ClaimTicket,
 			meta: {
-				isGuest: true,
+				takeItBackNowYall: true,
 			},
+		},
+		{
+			path: '/code-of-conduct',
+			name: 'code-of-conduct',
+			component: CodeOfConduct,
+		},
+
+		{
+			path: '/contact',
+			name: 'contact',
+			component: Contact,
+		},
+		{
+			path: '/edit-talk/:id',
+			name: 'edit-talk',
+			component: UserTalk,
+			meta: {
+				requiresAuth: true,
+			},
+		},
+		{
+			path: '/discount-tickets',
+			name: 'discount-tickets',
+			component: DiscountTickets,
+			meta: {
+				notYetAvailable: !ticketsOnSale,
+			},
+		},
+		{
+			path: '/events',
+			name: 'events',
+			component: Events,
+		},
+		{
+			path: '/letter-to-boss',
+			name: 'letter-to-boss',
+			component: LetterToBoss,
 		},
 		{
 			path: '/login',
@@ -58,6 +101,16 @@ const router = new Router({
 			meta: {
 				isGuest: true,
 			},
+		},
+		{
+			path: '/privacy-policy',
+			name: 'privacy-policy',
+			component: PrivacyPolicy,
+		},
+		{
+			path: '/profiles/:id',
+			name: 'profile',
+			component: ProfilePage,
 		},
 		{
 			path: '/profiles/me',
@@ -79,122 +132,12 @@ const router = new Router({
 			},
 		},
 		{
-			path: '/claim-ticket/:claimToken',
-			name: 'claim-ticket',
-			component: ClaimTicket,
+			path: '/register',
+			name: 'register',
+			component: Register,
 			meta: {
-				takeItBackNowYall: true,
+				isGuest: true,
 			},
-		},
-		{
-			path: '/events',
-			name: 'events',
-			component: Events,
-		},
-		/*
-		{
-			path: '/schedule',
-			name: 'schedule',
-			component: Schedule,
-		},
-		{
-			path: '/submit-talk',
-			name: 'submit-talk',
-			component: SubmitTalk,
-			meta: {
-				requiresAuth: true,
-				takeItBackNowYall: true,
-			},
-		},
-		*/
-		{
-			path: '/talks/:id',
-			name: 'talk',
-			component: TalkPage,
-		},
-		{
-			path: '/schedule/:id',
-			name: 'schedule-listing',
-			component: ScheduleListingPage,
-		},
-		{
-			path: '/workshops/:id',
-			name: 'workshop',
-			component: WorkshopPage,
-		},
-		/*
-		{
-			path: '/talks',
-			name: 'talks',
-			component: Talks,
-		},
-		*/
-		{
-			path: '/edit-talk/:id',
-			name: 'edit-talk',
-			component: UserTalk,
-			meta: {
-				requiresAuth: true,
-			},
-		},
-		{
-			path: '/conference-badges',
-			name: 'tickets',
-			component: Tickets,
-			meta: {
-				notYetAvailable: !ticketsOnSale,
-			},
-		},
-		{
-			path: '/student-tickets',
-			name: 'student-tickets',
-			component: StudentTickets,
-			meta: {
-				notYetAvailable: !ticketsOnSale,
-			},
-		},
-		{
-			path: '/discount-tickets',
-			name: 'discount-tickets',
-			component: DiscountTickets,
-			meta: {
-				notYetAvailable: !ticketsOnSale,
-			},
-		},
-		{
-			path: '/profiles/:id',
-			name: 'profile',
-			component: ProfilePage,
-		},
-		{
-			path: '/news',
-			name: 'news',
-			component: News,
-		},
-		{
-			path: '/about',
-			name: 'about',
-			component: About,
-		},
-		{
-			path: '/sponsorship',
-			name: 'sponsorship',
-			component: Sponsorship,
-		},
-		{
-			path: '/community',
-			name: 'community',
-			component: Community,
-		},
-		{
-			path: '/code-of-conduct',
-			name: 'code-of-conduct',
-			component: CodeOfConduct,
-		},
-		{
-			path: '/privacy-policy',
-			name: 'privacy-policy',
-			component: PrivacyPolicy,
 		},
 		{
 			path: '/reset-password',
@@ -207,15 +150,71 @@ const router = new Router({
 			component: RequestReset,
 		},
 		{
-			path: '/letter-to-boss',
-			name: 'letter-to-boss',
-			component: LetterToBoss,
-		}
+			path: '/schedule',
+			name: 'schedule',
+			component: Events,
+		},
+		/*
+		{
+			path: '/schedule',
+			name: 'schedule',
+			component: Schedule,
+		},
+		*/
+		{
+			path: '/schedule/:id',
+			name: 'schedule-listing',
+			component: ScheduleListingPage,
+		},
+		{
+			path: '/sponsorship',
+			name: 'sponsorship',
+			component: Sponsorship,
+		},
+
+		{
+			path: '/student-tickets',
+			name: 'student-tickets',
+			component: StudentTickets,
+			meta: {
+				notYetAvailable: !ticketsOnSale,
+			},
+		},
+		{
+			path: '/submit-talk',
+			name: 'submit-talk',
+			component: SubmitTalk,
+			meta: {
+				requiresAuth: true,
+				takeItBackNowYall: true,
+			},
+		},
+		{
+			path: '/talks',
+			name: 'talks',
+			component: Talks,
+		},
+		{
+			path: '/talks/:id',
+			name: 'talk',
+			component: TalkPage,
+		},
+		{
+			path: '/tickets',
+			name: 'tickets',
+			component: Tickets,
+			meta: {
+				notYetAvailable: !ticketsOnSale,
+			},
+		},
+		{
+			path: '/workshops/:id',
+			name: 'workshop',
+			component: WorkshopPage,
+		},
 	],
 	scrollBehavior(to) {
-		return to.hash
-			? { selector: to.hash }
-			: { x: 0, y: 0 };
+		return to.hash ? { selector: to.hash } : { x: 0, y: 0 };
 	},
 });
 
