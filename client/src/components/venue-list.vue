f9bec719cc2c8803119b346cdb8326a488baf297
Liz Henderson <lhenderson303@gmail.com>
2020-01-24 14:51:59 -0700

    location map on homepage

--- client/src/components/venue-list.vue
+++ client/src/components/venue-list.vue
@@ -5,19 +5,8 @@
 				title="Locations"
 				v-bind:imageUrl="require('@/assets/icons/happy_black.svg')"
 			/>
-			<div class="logos-row">
-				<a
-					v-for="venue in venues"
-					:key="venue.name"
-					:href="venue.linkUrl"
-					target="blank"
-				>
-					<img :src="venue.imageUrl" :alt="venue.name" />
-				</a>
-			</div>
-			<div class="map">
-				<LocationMap />
-			</div>
+
+			<LocationMap />
 		</section>
 	</div>
 </template>
@@ -26,46 +15,8 @@
 // Components
 import HeaderBar from '@/components/header-bar.vue';
 import LocationMap from '@/components/location-map.vue';
-// Logos
-import bigsbyLogo from '../assets/logos/bigsbys_folly_logo_small-black.png';
-import catalystLogo from '@/assets/logos/catalyst_logo_rgb_black.png';
-import helikonLogo from '../assets/logos/helikon_logo_final-black.png';
-import sourceLogo from '../assets/logos/source_hotel_logo_black.png';
-import zeppelinLogo from '@/assets/logos/zeppelin_station_logo_black.png';
 
 export default {
-	data() {
-		return {
-			venues: [
-				{
-					name: 'The Source Hotel',
-					linkUrl: 'https://thesourcehotel.com/',
-					imageUrl: sourceLogo,
-				},
-				{
-					name: 'Zeppelin Station',
-					linkUrl: 'https://zeppelinstation.com/',
-					imageUrl: zeppelinLogo,
-				},
-				{
-					name: 'Helikon Gallery',
-					linkUrl: 'https://helikongallery.com/',
-					imageUrl: helikonLogo,
-				},
-				{
-					name: 'Catalyst HTI',
-					linkUrl: 'http://www.catalysthealthtech.com/',
-					imageUrl: catalystLogo,
-				},
-
-				{
-					name: "Bigsby's Folly",
-					linkUrl: 'https://www.bigsbysfolly.com/',
-					imageUrl: bigsbyLogo,
-				},
-			],
-		};
-	},
 	components: {
 		HeaderBar,
 		LocationMap,
@@ -73,36 +24,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@import '@/styles/_flex.scss';
-@import '@/styles/_general.scss';
-@import '@/styles/_sizes.scss';
-
-section {
-	.logos-row {
-		@include flexbox;
-		grid-column: 1 / span 6;
-		grid-row: 2;
-		a {
-			@include flex(1);
-			margin: $baseline $baseline * 2;
-			img {
-				width: 16.66%;
-				min-width: 100px;
-			}
-		}
-		@media (max-width: $small-breakpoint) {
-			grid-column: 1;
-			justify-content: center;
-		}
-	}
-	.map {
-		grid-column: 1 / span 6;
-		grid-row: 3;
-		svg {
-			width: 100%;
-			height: auto;
-		}
-	}
-}
-</style>
+<style lang="scss"></style>
--- client/src/views/about.vue
+++ client/src/views/about.vue
@@ -1,29 +1,33 @@
 <template>
-    <section class="about">
+	<section class="about">
 		<div class="details">
 			<h2>About</h2>
-			<p>Behind the scenes is a group of friends who are passionate about building community and bringing people together. Our mission is to create something that empowers the makers and doers of the community.</p>
+			<p>
+				Behind the scenes is a group of friends who are passionate about
+				building community and bringing people together. Our mission is
+				to create something that empowers the makers and doers of the
+				community.
+			</p>
 		</div>
-    <div class="demo-reel">
-				<h2>2018 Demo Reel</h2>
-				<div class="video responsive-iframe-container">
-					<iframe
-						class="responsive-iframe"
-						src="https://www.youtube.com/embed/ZsRunRoI8Rk?controls=0&rel=0&showInfo=0"
-						frameborder="0"
-						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen
-					></iframe>
-				</div>
+		<div class="demo-reel">
+			<h2>2018 Demo Reel</h2>
+			<div class="video responsive-iframe-container">
+				<iframe
+					class="responsive-iframe"
+					src="https://www.youtube.com/embed/ZsRunRoI8Rk?controls=0&rel=0&showInfo=0"
+					frameborder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+				></iframe>
 			</div>
 		</div>
 		<profile-card-list :profiles="boardProfiles" />
-    </section>
+	</section>
 </template>
 
 <script>
-import ProfileCardList from "../components/profile-card-list";
-import boardProfiles from "../data/board-profiles";
+import ProfileCardList from '../components/profile-card-list';
+import boardProfiles from '../data/board-profiles';
 
 export default {
 	components: {
@@ -38,8 +42,8 @@ export default {
 </script>
 
 <style lang="scss">
-@import "@/styles/_sizes.scss";
-@import "@/styles/_typography.scss";
+@import '@/styles/_sizes.scss';
+@import '@/styles/_typography.scss';
 
 .about {
 }
