0e6aa7fafa3a31c3f3baa286baa2f061e47561ef
Liz Henderson <lhenderson303@gmail.com>
2020-02-10 21:06:41 -0700

    contact page

--- client/src/views/contact.vue
+++ client/src/views/contact.vue
@@ -5,10 +5,23 @@
 			<h1>Questions? Comments. Codeblocks.</h1>
 		</section>
 		<section class="contact">
-			<div class="details">
-				<h2>Contact</h2>
+			<HeaderBar
+				title="Contact"
+				v-bind:imageUrl="
+					require('@/assets/icons/DD_SITE_ICONS_SPONSORS.png')
+				"
+				v-bind:hoverUrl="
+					require('@/assets/icons/DD_HOVER_DVLP_DNVR.svg')
+				"
+			/>
+			<div class="intro">
+				<h4>Comments, Questions, Press</h4>
 				<a href="mailto:hello@developdenver.org"
 					>hello@developdenver.org</a
+				>
+				<h4>Sponsorship</h4>
+				<a href="mailto:sponsorship@developdenver.org"
+					>sponsorship@developdenver.org</a
 				>
 			</div>
 		</section>
@@ -18,11 +31,23 @@
 <script>
 import Vue from 'vue';
 import Fragment from 'vue-fragment';
+import HeaderBar from '@/components/header-bar.vue';
 import Countdown from '@/components/count-down.vue';
 export default {
 	components: {
 		Countdown,
+		HeaderBar,
 	},
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+@import '@/styles/_general.scss';
+.contact {
+	h4 {
+		padding: $baseline * 2 0 $baseline/2 0;
+		&:first-child {
+			padding-top: 0;
+		}
+	}
+}
+</style>
