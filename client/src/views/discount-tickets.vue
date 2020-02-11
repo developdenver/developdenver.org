bb1fe381c170fb7533afac03c84194e7565d3b34
Liz Henderson <lhenderson303@gmail.com>
2020-02-10 21:27:28 -0700

    wip discount-tickets

--- client/src/views/discount-tickets.vue
+++ client/src/views/discount-tickets.vue
@@ -1,37 +1,63 @@
 <template>
-	<section class="discount-tickets">
-		<countdown />
-		<h2>Tickets</h2>
-		<div class="ticket-details">
-			<div class="date-and-location">
+	<fragment>
+		<section id="tickets-landing" class="full landing-screen">
+			<countdown />
+			<h1>The hottest ticket in town</h1>
+			<div class="plus-grid red"></div>
+		</section>
+		<section class="discount-tickets">
+			<HeaderBar
+				title="The Details"
+				v-bind:imageUrl="
+					require('@/assets/icons/DD_SITE_ICONS_TIX.png')
+				"
+				v-bind:hoverUrl="
+					require('@/assets/icons/DD_HOVER_DVLP_DNVR.svg')
+				"
+			/>
+			<div class="ticket-details">
 				<p>When: August 15 and 16, 2019</p>
 				<p>Where: RiNo Brighton Boulevard</p>
-			</div>
-			<div class="benefits">
 				<p>
 					All badges include a custom printed 2-day conference badge,
 					access to keynote, talks, workshops, and live podcasts,
 					coffee, breakfast, and lunch at Zeppelin Station vendors
-					both days, opening and closing parties, 8th annual Ballmer
+					both days, opening and closing parties, 9th annual Ballmer
 					Peak Hackathon
 				</p>
 			</div>
-		</div>
-		<ticket-purchase :levels="levels" :isDiscountCode="true" />
-	</section>
+		</section>
+		<section id="buy-tickets">
+			<HeaderBar
+				title="Buy Tickets"
+				v-bind:imageUrl="
+					require('@/assets/icons/DD_SITE_ICONS_TIX.png')
+				"
+				v-bind:hoverUrl="
+					require('@/assets/icons/DD_HOVER_DVLP_DNVR.svg')
+				"
+			/>
+			<ticket-purchase :levels="levels" :isDiscountCode="true" />
+		</section>
+	</fragment>
 </template>
 
 <script>
+import Vue from 'vue';
+import Fragment from 'vue-fragment';
+
+import Countdown from '@/components/count-down';
+import HeaderBar from '@/components/header-bar.vue';
+import TicketPurchase from '../components/ticket-purchase';
 /* global Stripe */
 import CreditCardPayment from '../components/credit-card-payment';
-import TicketPurchase from '../components/ticket-purchase';
-import Countdown from '@/components/count-down';
 
 export default {
 	components: {
 		CreditCardPayment,
 		TicketPurchase,
 		Countdown,
+		HeaderBar,
 	},
 	data() {
 		return {
@@ -51,35 +77,33 @@ export default {
 @import '@/styles/_typography.scss';
 @import '@/styles/_general.scss';
 @import '@/styles/_sizes.scss';
-
-.discount-tickets {
-	@include grid;
+#tickets-landing {
+	.plus-grid.red {
+		@include plus-grid;
+		grid-column: 2 / span 4;
+		height: 30vh;
+		margin-top: 60vh;
+		position: absolute !important;
+		width: 50vw;
+		right: 0;
+		z-index: 1;
+	}
 	@media (max-width: $small-breakpoint) {
-		padding: $baseline;
-	}
-	.ticket-details {
-		@include grid-full-width;
-		@include grid;
-		.date-and-location {
-			@include grid-text-0;
+		grid-column: 1;
+		h1 {
+			grid-column: 1;
+			grid-row: 1;
 		}
-		.benefits {
-			@include grid-text-1;
-		}
-		.groups {
-			@include grid-text-2;
+		.plus-grid.red {
+			grid-column: 1;
+			grid-row: 2;
+			height: 55vh;
+			margin-top: 30vh;
 		}
 	}
-	.venues,
-	.ticket-purchase {
-		@include grid-full-width;
-	}
-	.ticket-details,
-	.venues {
-		margin-bottom: $baseline * 4;
-		@media (max-width: $small-breakpoint) {
-			margin-bottom: $baseline * 2;
-		}
-	}
+}
+
+.ticket-details {
+	@include grid-full-width;
 }
 </style>
